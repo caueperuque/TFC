8830296e25e625802c160f981e1ead02746a4adf
caue peruque <caue19052003@gmail.com>
2023-09-28 15:44:15 -0300

    feat: req16

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,8 +4,25 @@ import MatchesService from '../services/matches.service';
 class MatchesController {
   private matchesService: MatchesService = new MatchesService();
 
-  public async getAllMatches(_req: Request, res: Response) {
-    const matches = await this.matchesService.getAllMatches();
+  public async getAllMatches(req: Request, res: Response) {
+    const { inProgress } = req.query;
+
+    if (!inProgress) {
+      const matches = await this.matchesService.getAllMatches();
+      return res.status(200).json(matches.data);
+    }
+
+    return this.getAllMatchesByFilter(req, res);
+  }
+
+  public async getAllMatchesByFilter(req: Request, res: Response) {
+    const { inProgress } = req.query;
+    if (inProgress !== 'true' && inProgress !== 'false') {
+      return res.status(400).json({ message: 'Invalid query parameter' });
+    }
+
+    const response = JSON.parse(inProgress as string);
+    const matches = await this.matchesService.getMatchesByFilter(response);
 
     return res.status(200).json(matches.data);
   }
--- app/backend/src/services/matches.service.ts
+++ app/backend/src/services/matches.service.ts
@@ -5,14 +5,26 @@ class MatchesService {
   private matchesModel = MatchesModel;
 
   public async getAllMatches(): Promise<IServiceResponse<MatchesModel>> {
-    const match = await this.matchesModel.findAll({
+    const matches = await this.matchesModel.findAll({
       include: [
         { association: 'homeTeam', attributes: ['teamName'] },
         { association: 'awayTeam', attributes: ['teamName'] },
       ],
     });
 
-    return { status: 'SUCCESSFUL', data: match };
+    return { status: 'SUCCESSFUL', data: matches };
+  }
+
+  public async getMatchesByFilter(inProgress: boolean): Promise<IServiceResponse<MatchesModel>> {
+    const matches = await this.matchesModel.findAll({
+      where: { inProgress },
+      include: [
+        { association: 'homeTeam', attributes: ['teamName'] },
+        { association: 'awayTeam', attributes: ['teamName'] },
+      ],
+    });
+
+    return { status: 'SUCCESSFUL', data: matches };
   }
 }
 
