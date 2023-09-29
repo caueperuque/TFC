ea115e42092c0f2c18bf2b68e279187707afe739
caue peruque <caue19052003@gmail.com>
2023-09-29 15:18:49 -0300

    feat: req26 & 27 & 28

--- app/backend/src/routes/leaderboard.route.ts
+++ app/backend/src/routes/leaderboard.route.ts
@@ -7,5 +7,6 @@ const router = Router();
 const leaderboardController = new LeaderboardController();
 
 router.get('/home', (req, res) => leaderboardController.getHomeOrAway(req, res));
+router.get('/away', (req, res) => leaderboardController.getHomeOrAway(req, res));
 
 export default router;
